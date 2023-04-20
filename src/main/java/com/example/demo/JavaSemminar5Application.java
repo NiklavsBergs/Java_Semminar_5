ed06427645b5265a40d541bac278f53373cf8510
Niklāvs Bergs <s21bergnikl@vea.lv>
2023-04-20 15:24:51 +0300

    first commit

---
+++ src/main/java/com/example/demo/JavaSemminar5Application.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaSemminar5Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JavaSemminar5Application.class, args);
+	}
+
+}
