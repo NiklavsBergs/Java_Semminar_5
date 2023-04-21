df63a85a7e68065f35841565e77344927ce73b71
Niklāvs Bergs <s21bergnikl@vea.lv>
2023-04-21 14:37:15 +0300

    find product

--- src/main/java/com/example/demo/controllers/FirstController.java
+++ src/main/java/com/example/demo/controllers/FirstController.java
@@ -3,6 +3,9 @@ package com.example.demo.controllers;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestParam;
+
 import com.example.demo.models.Product;
 import java.util.ArrayList;
 import java.util.Arrays;
@@ -32,6 +35,27 @@ public class FirstController {
 		Product prod = new Product("Apple", "Tasty", 1.2f, 9);
 		model.addAttribute("packet", prod);
 		return "one-product-page";//will show one-product-page.html
+	}
+	
+	@GetMapping("/all-products")//localhost:8080/one-product
+	public String getAllProductFunc(Model model) {
+		model.addAttribute("packet", allProducts);
+		return "all-products-page";//will show all-products-page.html
+	}
+	
+	@GetMapping("/all-products-find")//localhost:8080/one-product
+	public String findProducts (@RequestParam("id") long id, Model model) {
+		if(id>0) {
+			for(Product temp : allProducts) {
+				if(temp.getId() == id) {
+					model.addAttribute("packet", temp);
+					return "one-product-page";
+				}
+			}
+		}
+		
+		model.addAttribute("packet-error", "Wrong ID");
+		return "error-page";//will show error-page.html
 	}
 
 }
